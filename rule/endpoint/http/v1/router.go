728a4aba7ad26f2745191d05e95ca5740b6064f0
jobs_vivi <jobs_vv@163.com>
2022-11-16 10:20:04 +0800

    :art: token

---
+++ rule/endpoint/http/v1/router.go
@@ -0,0 +1,34 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.kissvivi.kv-iot/rule/api"
+)
+
+type engine = *gin.Engine
+
+func InitRouter(api api.BaseApi) engine {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	gin.SetMode("debug")
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong this is kv-iot device endpoint"})
+	})
+
+	routers(r, api)
+
+	return r
+}
+
+func routers(r *gin.Engine, baseApi api.BaseApi) {
+	//g := r.Group("/test")
+	//{
+	//	//g.GET("/ok", baseApi)
+	//	//g.GET("/demo/ok", baseApi.DemoOk)
+	//	//g.POST("/create", baseApi.CreateTest)
+	//}
+}
